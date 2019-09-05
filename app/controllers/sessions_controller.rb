251eef36a9524c07ec25281d04137c91fa914a29
Will Howes <williameldenhowes@gmail.com>
2019-09-05 18:05:59 +0100

    Tests failing now
    Co-authored-by: <gennyallcroft@hotmail.co.uk>
    Co-authored-by: <richieganney@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
+
   def create
+
     user = User
             .find_by(email: params["name"]["email"])
             .try(:authenticate, params["email"]["password"])
